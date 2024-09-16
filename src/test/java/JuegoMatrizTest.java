9e350b0de149cab69ea8c17d6207072af0728ad6
FICLI32 <f.barriga02@ufromail.cl>
2024-09-16 18:40:28 -0300

    subo codigo del juego

---
+++ src/test/java/JuegoMatrizTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JuegoMatrizTest {
+
+    @Test
+    void estadisticasPesonaje() {
+
+    }
+
+    @Test
+    void estadisticasEnemigo() {
+
+    }
+
+    @Test
+    void movimientoPersonaje() {
+
+    }
+
+    @Test
+    void validarMovimineto() {
+    }
+
+    @Test
+    void estadoJuego() {
+    }
+}
